1b2ae2e66fd634a2f7bcca4e022ddeeff1ccde2a
strzcy <desipuspita46832@gmail.com>
2025-07-27 14:51:06 +0700

    tahap 4, dan tahap 5
    migrasi database, dan relasi model

--- app/Models/Nilai.php
+++ app/Models/Nilai.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Nilai extends Model
 {
-    //
+    use HasFactory;
+    protected$fillable = ['siswa_id', 'mapel', 'nilai'];
+
+    public function siswa(){
+        return$this->belongsTo(Siswa::class);
+    }
 }
--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Siswa extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable=['nama','kelas'];
+    
+    public function nilais(){
+        return $this->hasMany('Nilai::class');
+    }
 }
