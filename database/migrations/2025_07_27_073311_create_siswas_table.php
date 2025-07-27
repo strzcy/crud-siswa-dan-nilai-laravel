ebe3a27ae8e216b6374b6a535795bf5f5baa135e
strzcy <desipuspita46832@gmail.com>
2025-07-27 14:41:30 +0700

    Tahap 3
     Membuat struktur tabel di migration

--- database/migrations/2025_07_27_073311_create_siswas_table.php
+++ database/migrations/2025_07_27_073311_create_siswas_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('siswas', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('kelas');
             $table->timestamps();
         });
     }
--- database/migrations/2025_07_27_073314_create_nilais_table.php
+++ database/migrations/2025_07_27_073314_create_nilais_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('nilais', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('siswa_id')->constrained()->onDelete('cascade');
+            $table->integer('nilai');
+            $table->string('mapel');
             $table->timestamps();
         });
     }
